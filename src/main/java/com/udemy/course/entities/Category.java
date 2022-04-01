12a7429ee73d7ddd3047e28d80ce5727e67fb523
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-04-01 09:49:21 -0300

    FIX: hash code and equals the entity Category

--- src/main/java/com/udemy/course/entities/Category.java
+++ src/main/java/com/udemy/course/entities/Category.java
@@ -46,7 +46,7 @@ public class Category implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, name);
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -58,6 +58,6 @@ public class Category implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Category other = (Category) obj;
-		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+		return Objects.equals(id, other.id);
 	}
 }
